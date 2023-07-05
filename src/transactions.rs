afa6b024b49f13de3cc604388e743e6490490ca1
Aditya <aditya26sg@gmail.com>
2023-07-05 17:49:48 +0530

    update

--- src/transactions.rs
+++ src/transactions.rs
@@ -1,10 +1,8 @@
 use ethers::{
-    core::types::Bytes,
     providers::{Http, Middleware, Provider},
-    types::{TransactionRequest, H256},
+    types::{H256},
 };
-use ethers::abi::{Abi, Function};
-use hex::decode;
+use ethers::abi::{Abi, Function, Token};
 
 #[tokio::main]
 pub async fn get_transaction_data() {
@@ -70,18 +68,22 @@ pub async fn get_transaction_data() {
         .await
         .expect("Couldn't get the transaction receipt");
 
-    // println!("The transaction is {:?}", transaction);
-    // println!("------------------------------------------------------");
-    // println!("The transaction receipt is {:?} ", transaction_receipt);
+    let input_data: String = transaction.unwrap().input.to_string();
+    let input_data = &input_data[10..]; // removing the transaction hash
 
-    let input_data = transaction.unwrap().input;
-    println!("The inputs are {:?}", input_data);
-
-    let sample_input = "0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000001a8dc00000000000000000000000000000000000000000000000000000000000000c000000000000000000000000054325d507ed1b3776c146700eff61a98b45aec76000000000000000000000000000000000000000000000000000000000000012000000000000000000000000017317f96f0c7a845ffe78c60b10ab15789b57aaa0000000000000000000000000000000000000000000000000000000000000220000000000000000000000000000000000000000000000000000000000000003061723a2f2f337550425559524a424863383849376d5f676c6267626777613135694f473251514e4b3663317653702d380000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000e0000000000000000000000000000000000000000000000000016345785d8a000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000d500b1d8e8ef31e21c99d1db9a6444d3adf1270000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000321737a45f7942078ea5ad9d41ec9d163f16813a0000000000000000000000000000000000000000000000000000000000000000";
-    // let sample_input_bytes = decode(sample_input).expect("Failed to decode ");
-
-    let input_bytes = hex::decode(sample_input).expect("Failed to decode input bytes");
-    println!("Here are the input bytes {:?}", input_bytes);
+    let input_bytes = hex::decode(input_data).expect("Failed to decode input bytes");
     let decoded_inputs = function.decode_input(&input_bytes).expect("failed to decode inputs");
-    println!("The inputs are {:?}", decoded_inputs);
+    println!("The decoded inputs are {:?}", decoded_inputs);
+
+    // let param1: u128 = ethers::abi::Token::into(decoded_inputs[0]);
+
+
+    /// problem starts here below
+
+    let param1: u64 = match &decoded_inputs[0] {
+        Token::Uint(value) => value.to_owned().as_u64(), _ => panic!("unexpected token type"),
+    };
+
+    println!("The param 1 is {:?}", param1);
+    // println!("First Param {:?}", ethers::abi::Token::into(decoded_inputs[0].clone()));
 }
