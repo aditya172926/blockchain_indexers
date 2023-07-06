9a86ed53886e93ea2d03fcd4a21a276d2ed253a3
Aditya <aditya26sg@gmail.com>
2023-07-07 03:05:45 +0530

    refactoring

--- src/transactions.rs
+++ src/transactions.rs
@@ -1,62 +1,26 @@
+use ethers::abi::{Abi, Function, Token};
 use ethers::{
     providers::{Http, Middleware, Provider},
-    types::{H256, Address},
+    types::{Address, H256},
 };
-use ethers::abi::{Abi, Function, Token};
-
 
 #[tokio::main]
-pub async fn get_transaction_data(abi: Abi, function_name: String, transaction_hash: String) {
-    let abi_json = r#"[{
-        "inputs": [
-          {
-            "components": [
-              { "internalType": "uint256", "name": "profileId", "type": "uint256" },
-              { "internalType": "string", "name": "contentURI", "type": "string" },
-              {
-                "internalType": "address",
-                "name": "collectModule",
-                "type": "address"
-              },
-              {
-                "internalType": "bytes",
-                "name": "collectModuleInitData",
-                "type": "bytes"
-              },
-              {
-                "internalType": "address",
-                "name": "referenceModule",
-                "type": "address"
-              },
-              {
-                "internalType": "bytes",
-                "name": "referenceModuleInitData",
-                "type": "bytes"
-              }
-            ],
-            "internalType": "struct DataTypes.PostData",
-            "name": "vars",
-            "type": "tuple"
-          }
-        ],
-        "name": "post",
-        "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      }]"#;
-
-    let abi: Abi = serde_json::from_str(abi_json).expect("Failed to parse abi");
-    let function: &Function = abi.function("post").expect("Function is not found in ABI");
+pub async fn get_transaction_data(abi: &str, function_name: String, transaction_hash: String) {
+    let abi: Abi = serde_json::from_str(abi).expect("Failed to parse abi");
+    println!("The decoded abi is {:?}", abi);
+    let function: &Function = abi
+        .function(&function_name)
+        .expect("Function is not found in ABI");
 
     println!("Running the transactions rust file {:?}", function.inputs);
     // connect to a network -> eg Polygon
     let provider = Provider::<Http>::try_from("https://rpc-mainnet.maticvigil.com")
         .expect("Failed to connect with a Provider");
+    println!("GOt the provider {:?}", provider);
 
-    let transaction_hash: H256 =
-        "0x6b69174c0969eda83feb75734fee22722b518aba79be76aaa839ae58fd44d58b"
-            .parse()
-            .expect("Failed to parse transaction hash");
+    let transaction_hash: H256 = transaction_hash
+        .parse()
+        .expect("Failed to parse transaction hash");
 
     // getting the transaction details
     let transaction = provider
@@ -73,33 +37,36 @@ pub async fn get_transaction_data(abi: Abi, function_name: String, transaction_h
     let input_data = &input_data[10..]; // removing the transaction hash
 
     let input_bytes = hex::decode(input_data).expect("Failed to decode input bytes");
-    let decoded_inputs = function.decode_input(&input_bytes).expect("failed to decode inputs");
+    let decoded_inputs = function
+        .decode_input(&input_bytes)
+        .expect("failed to decode inputs");
     println!("The decoded inputs are {:?}", decoded_inputs);
 
-      
     let vec_tokens = match &decoded_inputs[0] {
-      Token::Tuple(tokens) => tokens.to_owned(),
-      _ => panic!("Unexpected token type"),
+        Token::Tuple(tokens) => tokens.to_owned(),
+        _ => panic!("Unexpected token type"),
     };
-  
+
     println!("The vec tokens are {:?}", vec_tokens);
 
     let param1: u64 = match &vec_tokens[0] {
-        Token::Uint(value) => value.to_owned().as_u64(), _ => panic!("unexpected token type"),
+        Token::Uint(value) => value.to_owned().as_u64(),
+        _ => panic!("unexpected token type"),
     };
 
     println!("The param 1 is {:?}", param1);
 
     let param2: String = match &vec_tokens[1] {
-      Token::String(value) => value.to_owned().to_string(), _ => panic!("unexpected token type"),
+        Token::String(value) => value.to_owned().to_string(),
+        _ => panic!("unexpected token type"),
     };
 
     println!("The param 2 is {:?}", param2);
 
     let param3: Address = match &vec_tokens[2] {
-      Token::Address(value) => value.to_owned(), _ => panic!("unexpected token type"),
+        Token::Address(value) => value.to_owned(),
+        _ => panic!("unexpected token type"),
     };
 
     println!("The param 3 is {:?}", param3);
-    
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,12 @@
+use ethers::providers::Provider;
+use std::fmt;
 use std::fs;
 use std::string::String;
-use std::fmt;
-use ethers::providers::Provider;
+use url::ParseError;
 
 pub fn get_network_rpc(network_id: &str) -> String {
-    let network_details: String = fs::read_to_string(r"config/network.json").expect("Error in reading network.json file");
+    let network_details: String =
+        fs::read_to_string(r"config/network.json").expect("Error in reading network.json file");
     let network_details = serde_json::from_str::<serde_json::Value>(&network_details);
     let network_rpc = match network_details {
         Ok(object) => object[network_id]["networkRpcUrl"].to_string(),
@@ -16,7 +18,8 @@ pub fn get_network_rpc(network_id: &str) -> String {
 }
 
 pub fn get_contract_metadata(protocol_name: &str) -> String {
-    let contract_meta_data: String = fs::read_to_string(r"config/global.json").expect("Error in reading global.json file");
+    let contract_meta_data: String =
+        fs::read_to_string(r"config/global.json").expect("Error in reading global.json file");
     let contract_meta_data = serde_json::from_str::<serde_json::Value>(&contract_meta_data);
     let contract_chain_id: String;
     let mut contract_address: String = "".to_string();
@@ -33,13 +36,33 @@ pub fn get_contract_metadata(protocol_name: &str) -> String {
     return contract_address;
 }
 
-pub async fn get_contract_instance(network_rpc: String) {
-    let provider = Provider::try_from(network_rpc);
+// pub async fn get_provider(
+//     chain_id: String,
+// ) -> std::result::Result<Provider<ethers::providers::Http>,  ethers::abi::ParseError> {
+//     let network_data: String =
+//         fs::read_to_string(r"config/network.json").expect("Error in reading network.json file");
+//     let network_data = serde_json::from_str::<serde_json::Value>(&network_data);
+//     let mut network_rpc: String = String::new();
+//     match network_data {
+//         Ok(object) => {
+//             network_rpc = object[chain_id]["networkRpcUrl"].to_string();
+//             let provider = Provider::try_from(network_rpc);
+//             println!("The provider is {:?}", provider);
+//             return provider;
+//         }
+//         Err(e) => {
+//             println!("Error in reading networkRpcUrl {:?}", e);
+//             return ethers::abi::ParseError::ParseError(e);
+//         }
+//     }
+// }
 
-}
-
-pub async fn fetch_contract_abi(network_name: String, contract_address: &str) -> reqwest::Result<reqwest::Response> {
-    let file: String = fs::read_to_string(r"config/constants.json").expect("Error in reading the constants.json file");
+pub async fn fetch_contract_abi(
+    network_name: String,
+    contract_address: &str,
+) -> reqwest::Result<reqwest::Response> {
+    let file: String = fs::read_to_string(r"config/constants.json")
+        .expect("Error in reading the constants.json file");
     let file_data = serde_json::from_str::<serde_json::Value>(&file);
 
     let mut api: String = String::new();
@@ -53,7 +76,7 @@ pub async fn fetch_contract_abi(network_name: String, contract_address: &str) ->
     }
 
     let mut api_url = str::replace(&api, "{}", &contract_address);
-    api_url = api_url[1..api_url.len()-1].to_string();
+    api_url = api_url[1..api_url.len() - 1].to_string();
     println!("The api_url is {}", api_url);
 
     let response: Result<reqwest::Response, reqwest::Error> = reqwest::get(&api_url).await;
