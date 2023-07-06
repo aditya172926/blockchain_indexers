7bea7110bc220f7c73a4ca55703dfc79d14e2189
Varun Doshi <varun@metaworkhq.com>
2023-07-06 05:50:02 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use ethcontract::prelude::*;
 use ethcontract::{
     contract,
-    web3::types::{BlockNumber, H160, H256},
-    Address
 }; 
+use ethers::{
+    contract::Contract,
+    types::{Address, U256,H160},
+    abi::Abi
+};
+
+use ethers::providers::Provider;
 use serde::{Deserialize, Serialize};
 use sqlx::postgres::{PgPoolOptions, PgRow};
 use sqlx::{FromRow, Row};
 use std::fs;
 use std::string::String;
+use std::sync::Arc;
 use std::{error::Error, str::FromStr};
 use tokio::time::{sleep, Duration};
 use web3::transports::Http;
@@ -55,9 +61,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         Address::from_str(&contractAddress).expect("Failed to convert to address type");
 
     // temporarily commenting the get_logs function
-    // get_logs(&networkEndpoint, contractAddress);
+    println!("--------------------------------------------------------------");
+    get_logs(&networkEndpoint, contractAddress);
 
-    transactions::get_transaction_data();
+    // transactions::get_transaction_data();
 
     Ok(())
 }
@@ -65,33 +72,79 @@ fn main() -> Result<(), Box<dyn Error>> {
 //Function to get Logs of events
 #[tokio::main]
 async fn get_logs(networkEndpoint: &str, contractAddress: Address) -> Result<(), Box<dyn Error>> {
-    let transport = Http::new(&networkEndpoint)?;
-    let web3 = Web3::new(transport);
-    let contract_address: H160 = contractAddress;
+    // let transport = Http::new(&networkEndpoint)?;
+    // let web3 = Web3::new(transport);
+    // let contract_address: H160 = contractAddress;
+    let rpc_url = "https://lingering-delicate-choice.discover.quiknode.pro/68f9e3726efe97ee2b6a7c8417f6f5d12ab713c6/";
+    let provider = Provider::try_from(rpc_url)?;
+
+
+    let etherscan_api_token="ER9VKT8AXAI2WTPSCRNANN69W67V7PRU59".to_string();
+    let contract_address = "0x231d3559aa848Bf10366fB9868590F01d34bF240".to_string();
+
+
+    let api_url=format!("https://api.etherscan.io/api?module=contract&action=getabi&address={}&apikey={}",contract_address,etherscan_api_token);
+
+    let response = reqwest::get(&api_url).await?;
+
+    let mut fetched_abi="na".to_string();
+
+
+    if response.status().is_success() {
+
+        // Read the response body as a string
+
+        let response_body = response.text().await?;
+
+        // Parse the response body as JSON
+        let json: serde_json::Value = serde_json::from_str(&response_body)?;
+        
+        fetched_abi=json["result"].as_str().unwrap().to_owned();
+    } else {
+        println!("Request failed with status code: {}", response.status());
+    }
+
+    let abi: Abi = serde_json::from_str(&fetched_abi).unwrap();
+    let address:H160=contract_address.parse()?;
+
+
+    let contract_instance=Contract::new(address,abi,Arc::new(provider.clone()));
+    println!("{:?}",contract_instance);
+
+
 
     // let _ = fetch_ens_name().await;
-    let contract_abi = include_bytes!("../abi/abi_1.json");
-    let contract = ENSRegistryWithFallback::at(&web3, contract_address);
-    println!("Contract Abi {:?}", contract);
+    // let contract_abi = include_bytes!("../abi/abi_1.json");
+    // let contract = ENSRegistryWithFallback::at(&web3, contract_address);
+
+    println!("Contract Abi {:?}", address);
     println!("--------------------------------------------------------");
-    println!("1. Contract initialized {:?}", contract);
+    println!("1. Contract initialized {:?}", contract_instance);
     // let contract2: Instance = Instance::with_deployment_info(&web3, contract_abi.to_vec(), contract_address, None);
     // Subscribe to all events
-    let mut event_streams = contract
-        .events()
-        .new_owner()
-        .stream()
-        .boxed();
+    // let mut event_streams = contract_instance
+    //     .events()
+    //     .new_owner()
+    //     .stream()
+    //     .boxed();
 
-    loop {
-        join! {
-            async {
-                let log = event_streams.next().await.expect("No events").expect("Error querying event").added();
-                let unwraped_log = log.unwrap();
-                println!("Received a new event log {:?}", unwraped_log.owner);
-            },
-        };
-    }
+    let logs = contract_instance
+    .event()
+    .from_block(0u64)
+    .query()
+    .await?;
+
+// println!("{:?}", logs);
+
+    // loop {
+    //     join! {
+    //         async {
+    //             let log = event_streams.next().await.expect("No events").expect("Error querying event").added();
+    //             let unwraped_log = log.unwrap();
+    //             println!("Received a new event log {:?}", unwraped_log.owner);
+    //         },
+    //     };
+    // }
 
     Ok(())
 }
